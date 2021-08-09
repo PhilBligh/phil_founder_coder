50a8fca1147b8f1860ec42ef07504554433b0ecc
Phil Bligh <blighp01@Phils-MacBook-Pro.local>
2021-08-09 09:33:58 +0100

    Finished version 1

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ All code was written by me and no external libraries were used. My knowledge was
 
 Each section, including the nav bar is its own flexbox. I used flex for the ease of arranging the items within the flexbox to create a hopefully more tidy display. 
 
-The 'Other Interests' and 'Links' section use DOM event listeners and DOM updates, in the case of 'Other Interests' to scroll through a photo gallery, and in the case of 'Links' to darken the link image when the mouse hovers over it.
+The 'Other Interests' and 'Links' section use DOM event listeners and DOM updates, in the case of 'Other Interests' to scroll through a photo gallery, and in the case of 'Links' to lower the opacity of the link image when the mouse hovers over it.
 
 I hope this meets the requirements and I hope I might be hearing from you in the future.
 
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Interests section
+
 let prev1 = document.getElementById('prev1')
 let next1 = document.getElementById('next1')
 let prev2 = document.getElementById('prev2')
@@ -7,16 +9,6 @@ let next3 = document.getElementById('next3')
 let japanese = document.getElementById('japanese')
 let writing = document.getElementById('writing')
 let family = document.getElementById('family')
-
-console.log(prev1)
-console.log(next1)
-console.log(prev2)
-console.log(next2)
-console.log(prev3)
-console.log(next3)
-console.log(japanese)
-console.log(writing)
-console.log(family)
 
 
  let buttonClick = function(section1, section2) {
@@ -31,5 +23,43 @@ next2.addEventListener('click', e =>{buttonClick(family, writing)})
 prev3.addEventListener('click', e =>{buttonClick(writing, family)})
 next3.addEventListener('click', e =>{buttonClick(japanese, family)})
 
+// Links section
 
+let github = document.getElementById('github')
+let codewars = document.getElementById('codewars')
+let freecodecamp = document.getElementById('freecodecamp')
+let readme = document.getElementById('readme')
 
+/*let hoverLink = function(link) {
+    link.style.opacity=0.5;
+}
+prev1.addEventListener('click', e =>{buttonClick(family, japanese)})
+*/
+
+github.onmouseover = function() {
+    github.style.opacity = 0.5;
+  }
+  github.onmouseout = function() {
+    github.style.opacity = 1;
+  }
+
+  codewars.onmouseover = function() {
+    codewars.style.opacity = 0.5;
+  }
+  codewars.onmouseout = function() {
+    codewars.style.opacity = 1;
+  }
+
+  freecodecamp.onmouseover = function() {
+    freecodecamp.style.opacity = 0.5;
+  }
+  freecodecamp.onmouseout = function() {
+    freecodecamp.style.opacity = 1;
+  }
+
+  readme.onmouseover = function() {
+    readme.style.opacity = 0.5;
+  }
+  readme.onmouseout = function() {
+    readme.style.opacity = 1;
+  }
